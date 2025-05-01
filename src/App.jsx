cbaa8afe5802a5fdb506290a561b573cc1e45992
Rerigan <cosnnum@gmail.com>
2025-05-01 00:32:49 -0300

    potential fix (app.jsx change)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import NavBar from "./components/NavBar";
 import { MovieProvider } from "./contexts/MovieContext";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
