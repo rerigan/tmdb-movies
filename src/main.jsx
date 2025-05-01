a85a15581e1aeb13893c20f10747981c3107f61f
Rerigan <cosnnum@gmail.com>
2025-04-30 21:18:40 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./css/index.css";
 import App from "./App.jsx";
-
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/tmdb-movies" sensitive={false}>
