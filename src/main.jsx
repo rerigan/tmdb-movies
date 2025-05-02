bf174fa25dadf3f81e79a182d9d55b3787a6d1ab
Rerigan <cosnnum@gmail.com>
2025-05-02 17:49:16 -0300

    fully working, favorites save, 404 doesn't happen anymore

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
 import App from "./App.jsx";
-import { HashRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";"react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/tmdb-movies" sensitive={false}>
+    <HashRouter  sensitive={false}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
