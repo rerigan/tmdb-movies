f0f7e09485a89d432f87c1432427dcbf8239bdc1
Rerigan <cosnnum@gmail.com>
2025-05-06 08:27:19 -0300

    search button fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host:true
+  },
   plugins: [react()],
   base: "/tmdb-movies"
 })
